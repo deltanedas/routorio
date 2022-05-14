b0541a52946e265e84143966f61cb58a95d68a99
DeltaNedas <deltanedas@gmail.com>
2022-05-14 17:14:51 +0100

    crash

--- src/routorio/content/RLiquids.java
+++ src/routorio/content/RLiquids.java
@@ -7,13 +7,13 @@ public class RLiquids {
 	public static Liquid liquidRouter, fusionMix;
 
 	public static void load() {
-		liquidRouter = new Liquid("liquid-router", Color.valueOf("#6e7080")) {{
+		liquidRouter = new Liquid("liquid-router", Color.valueOf("6e7080")) {{
 			heatCapacity = 0.1f;
 			temperature = 1.1f;
 			viscosity = 1f;
 		}};
 
-		fusionMix = new Liquid("fusion-mix", Color.valueOf("")) {{
+		fusionMix = new Liquid("fusion-mix", Color.valueOf("390e04")) {{
 			heatCapacity = 0.1f;
 			temperature = 3.0f;
 			viscosity = 0.2f;
